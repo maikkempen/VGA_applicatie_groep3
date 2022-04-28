6fabd39dc6cd52d603f119c1e0bf631d6d00f3c6
maikkempen <maikkempen171@gmail.com>
2022-04-28 13:39:45 +0200

    init error handler files

---
+++ Lib/Errorhandler/errorhandler.c
@@ -0,0 +1,13 @@
+/**
+  ******************************************************************************
+  * @file      errorhandler.c
+  * @author    Maik Kempen
+  * @brief     Error handler for detection of syntax, logic
+  *            and hardware errors and notification specified to users
+  *            (programmer, end-user)
+  ******************************************************************************
+  */
+
+
+
+ 
