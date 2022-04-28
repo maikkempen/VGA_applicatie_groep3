90db0be74a64d436e8576f5a415d100dda80489a
maikkempen <maikkempen171@gmail.com>
2022-04-28 20:11:00 +0200

    add struct with line-command

--- Lib/Parser/parser.h
+++ Lib/Parser/parser.h
@@ -14,12 +14,26 @@
 #include "usart.h"
 
 /* DEFINES ******************************/
-
+#define LINE_CMD_ID 1   /*!< line command ID */
 /* ENUMS ******************************/
 
 /* STRUCTS ******************************/
 
 /*! struct for storing command data front layer to use in logic layer */
+typedef struct
+{
+  int8_t ID;  /*!< ID for command type */
+  /*! struct for storing line-command arguments */
+  struct line
+  {
+    uint16_t x1;        /*!< first x coordinate */
+    uint16_t y1;        /*!< first y coordinate */
+    uint16_t x2;        /*!< second x coordinate */
+    uint16_t y2;        /*!< second y coordinate */
+    uint8_t color;      /*!< line color (8-bit color value) */
+    uint8_t weight;     /*!< weight (thickness) of line in px */
+  };
+} COMMAND;
 
 /* EXTERN VARIABLES ******************************/
 
