4df12ca599d34c191674314bb36b4d23fec7d2b4
maikkempen <maikkempen171@gmail.com>
2022-04-28 20:10:11 +0200

    init parser files

---
+++ Lib/Parser/parser.c
@@ -0,0 +1,9 @@
+/**
+  ******************************************************************************
+  * @file      parser.c
+  * @author    Max Besseling
+  * @brief     Front layer parser to analyze and store command syntax to be used
+  *            by logic layer
+  ******************************************************************************
+  */
+
---
+++ Lib/Parser/parser.h
@@ -0,0 +1,30 @@
+/**
+  ******************************************************************************
+  * @file      parser.h
+  * @author    Max Besseling, Maik Kempen
+  * @brief     header file for parser.c
+  ******************************************************************************
+  */
+
+#ifndef PARSER_H_
+#define PARSER_H_
+ 
+ /* INCLUDES ******************************/
+#include "main.h"
+#include "usart.h"
+
+/* DEFINES ******************************/
+
+/* ENUMS ******************************/
+
+/* STRUCTS ******************************/
+
+/*! struct for storing command data front layer to use in logic layer */
+
+/* EXTERN VARIABLES ******************************/
+
+/* PROTOTYPES ******************************/
+
+ 
+ #endif // PARSER_H_
+ 
