e5182113f75c16f1c269978f45228ddba1c1b671
maikkempen <maikkempen171@gmail.com>
2022-05-15 20:22:10 +0200

    fix prototype param names IO_drawRectangle

--- Lib/IO_layer_Lib/IO_func.h
+++ Lib/IO_layer_Lib/IO_func.h
@@ -33,7 +33,7 @@ uint8_t IO_drawLine(uint16_t x0, uint16_t y0, uint16_t x1, uint16_t y1, uint8_t 
 void _swap_int16_t (uint16_t *a, uint16_t *b);
 void IO_clearScreen(uint8_t color);
 void IO_drawBitmap(uint8_t nr, int16_t x1, int16_t y1, uint8_t color);
-void IO_drawRectangle(uint16_t x_lup, uint16_t y_lup, uint16_t breedte, uint16_t hoogte, uint8_t kleur, uint16_t gevuld);
+uint8_t IO_drawRectangle(uint16_t x_lup, uint16_t y_lup, uint16_t width, uint16_t height, uint8_t color, uint16_t filled);
 int8_t findGlyph(char c);
 void IO_drawText(uint16_t x1, uint16_t y1, uint8_t color, uint8_t *textString, uint8_t fontName, uint8_t fontSize, uint8_t fontStyle);
 
